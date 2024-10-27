0c26522100b84e18e3e7ddc0b71349a252208af5
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-27 21:53:30 +0200

    Первое успешное добавление

--- CallingFunctions/call_octal.py
+++ CallingFunctions/call_octal.py
@@ -1,5 +1,11 @@
-def to_binary(octal : str):
-    temp = int(octal, 8)
-    result = bin(temp)[2:]
+import ctypes
+
+def to_binary(transformation):
+    lib = ctypes.CDLL(r"C++\translationLibrary.dll")
     
-    return str(result)
+    lib.octalToBinary.argtypes = [ctypes.c_char_p]
+    lib.octalToBinary.restype = ctypes.c_char_p
+    
+    result = lib.octalToBinary(transformation.encode('utf-8'))
+    
+    return result.decode('utf-8')
