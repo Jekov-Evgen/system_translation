898c49abee0bac88e292865ac3c681b27db9ac2b
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-27 22:19:59 +0200

    Создали для двоичной системы счисления

--- CallingFunctions/call_binary.py
+++ CallingFunctions/call_binary.py
@@ -1,1 +1,31 @@
 import ctypes
+
+def to_octal(transformation):
+    lib = ctypes.CDLL(r"C++\translationLibrary.dll")
+    
+    lib.binaryToOctal.argtypes = [ctypes.c_char_p]
+    lib.binaryToOctal.restype = ctypes.c_char_p
+    
+    result = lib.binaryToOctal(transformation.encode('utf-8'))
+    
+    return result.decode('utf-8')
+
+def to_decimal(transformation):
+    lib = ctypes.CDLL(r"C++\translationLibrary.dll")
+    
+    lib.binaryToDecimal.argtypes = [ctypes.c_char_p]
+    lib.binaryToDecimal.restype = ctypes.c_char_p
+    
+    result = lib.binaryToDecimal(transformation.encode('utf-8'))
+    
+    return result.decode('utf-8')
+
+def to_hex(transformation):
+    lib = ctypes.CDLL(r"C++\translationLibrary.dll")
+    
+    lib.binaryToHexadecimal.argtypes = [ctypes.c_char_p]
+    lib.binaryToHexadecimal.restype = ctypes.c_char_p
+    
+    result = lib.binaryToHexadecimal(transformation.encode('utf-8'))
+    
+    return result.decode('utf-8')
--- GUI/from_binary.py
+++ GUI/from_binary.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QLabel, QVBoxLayout, QPushButton, QMainWindow, QLineEdit, QWidget, QMessageBox
 from GUI.style import CONST_WINDOW
+from CallingFunctions.call_binary import to_octal, to_decimal, to_hex
 
 class FromBinary(QMainWindow):
     def __init__(self) -> None:
@@ -13,7 +14,7 @@ class FromBinary(QMainWindow):
         central_widget = QWidget()
         
         instructions = QLabel(text="Введите ваше бинарное число для перевода")
-        entering_a_number = QLineEdit()
+        self.entering_a_number = QLineEdit()
         
         octal = QPushButton(text="Из двоичного в восьмиричное")
         octal.clicked.connect(self.to_octal)
@@ -25,7 +26,7 @@ class FromBinary(QMainWindow):
         hexadecimal.clicked.connect(self.to_hexadecimal)
         
         control_UI.addWidget(instructions)
-        control_UI.addWidget(entering_a_number)
+        control_UI.addWidget(self.entering_a_number)
         control_UI.addWidget(octal)
         control_UI.addWidget(decimal)
         control_UI.addWidget(hexadecimal)
@@ -37,23 +38,32 @@ class FromBinary(QMainWindow):
         self.show()
         
     def to_octal(self):
+        temp = self.entering_a_number.text()
+        call = to_octal(temp)
+        
         self.result = QMessageBox()
         self.result.setWindowTitle("Окно результата")
-        self.result.setText("Результат из двоичного в восьмиричное")
+        self.result.setText(call)
         
         self.result.show()
         
     def to_decimal(self):
+        temp = self.entering_a_number.text()
+        call = to_decimal(temp)
+        
         self.result = QMessageBox()
         self.result.setWindowTitle("Окно результата")
-        self.result.setText("Результат из двоичного в десятичное")
+        self.result.setText(call)
         
         self.result.show()
         
     def to_hexadecimal(self):
+        temp = self.entering_a_number.text()
+        call = to_hex(temp)
+        
         self.result = QMessageBox()
         self.result.setWindowTitle("Окно результата")
-        self.result.setText("Результат из двоичного в шестнадцатеричное")
+        self.result.setText(call)
         
         self.result.show()
         
